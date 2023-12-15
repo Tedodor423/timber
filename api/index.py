dcd435ab2a9af7e34b2632f07120368bcb982101
Tedodor423 <vrtulnik1@gmail.com>
2023-12-15 17:24:02 +0100

    nicepage test

--- api/index.py
+++ api/index.py
@@ -1,10 +1,30 @@
-from flask import Flask
-
+# init Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# init AI
+from openai import OpenAI
+client = OpenAI()
+client = OpenAI(api_key="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")
+
+# completion = client.chat.completions.create(
+#  model="gpt-3.5-turbo",
+#  messages=[
+#    {"role": "system", "content": "You are a christmas tree on a dating site like Tinder"},
+#    {"role": "user", "content": "Write your bio for a dating site like tinder, which includes your first name, age as a number, height as a number, hobbies, tree type. make it short, each information on a separate line, use emojis before each title"}
+#  ]
+# )
+
+# print(completion.choices[0].message.content)
+
+
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+
+    page = render_template("index.html", tree="StromZivota")
+
+    return page
 
 @app.route('/about')
 def about():
